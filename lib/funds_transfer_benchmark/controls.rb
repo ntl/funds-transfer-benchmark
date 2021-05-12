ead3bea499847559109ffc8c297849b181859577
Nathan Ladd <nathanladd@gmail.com>
2021-05-11 21:54:14 -0500

    Account and FundsTransfer ID controls

--- lib/funds_transfer_benchmark/controls.rb
+++ lib/funds_transfer_benchmark/controls.rb
@@ -3,3 +3,6 @@ require 'identifier/uuid'
 require 'funds_transfer_benchmark/controls/id'
 
 require 'funds_transfer_benchmark/controls/stream_name'
+
+require 'funds_transfer_benchmark/controls/account'
+require 'funds_transfer_benchmark/controls/funds_transfer'
---
+++ lib/funds_transfer_benchmark/controls/account.rb
@@ -0,0 +1,25 @@
+module FundsTransferBenchmark
+  module Controls
+    module Account
+      module ID
+        def self.example(increment=nil, seed: nil)
+          Controls::ID.example(increment, prefix: prefix, seed: seed)
+        end
+
+        def self.prefix
+          0x00000000
+        end
+
+        module Sequence
+          def self.example(count=nil, partitions: nil)
+            Controls::ID::Sequence.example(count, prefix: prefix, partitions: partitions)
+          end
+
+          def self.prefix
+            ID.prefix
+          end
+        end
+      end
+    end
+  end
+end
